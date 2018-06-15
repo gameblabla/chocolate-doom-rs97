79c4db19cafbd7a22c0ee2b41fd6e82a258e90d0
gameblabla <gameblabla@openmailbox.org>
2018-06-15 13:28:40 -0400

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+PATH=/opt/rs97-toolchain/bin:$PATH
+./configure --host=mipsel-linux SDL_CONFIG=/opt/rs97-toolchain/mipsel-buildroot-linux-uclibc/sysroot/usr/bin/sdl-config CFLAGS="-Ofast -mips32 -mno-mips16 -mhard-float -DUSE_VIRTUALKEYBOARD" LDFLAGS="-s"
